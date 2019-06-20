e49b6ead5fbb7a2e77c3967b625164fc3d96c4f7
Panu L <panu.linnal4@gmail.com>
2019-06-20 18:26:29 +0300

    Add SEO to info page

--- src/pages/info/index.jsx
+++ src/pages/info/index.jsx
@@ -4,22 +4,26 @@ import MembershipCard from "../../components/MembershipCard.jsx";
 import DocumentsCard from "../../components/DocumentsCard.jsx";
 import ContactCard from "../../components/ContactCard.jsx";
 import CurrentBoardCard from "../../components/CurrentBoardCard.jsx";
+import SEO from "../../components/SEO.jsx";
 
 const InfoPage = () => (
-    <Layout>
-        <div className="container">
-            <MembershipCard />
-            <CurrentBoardCard />
-            <div className="flex flex-wrap">
-                <div className="w-full md:w-1/2">
-                    <DocumentsCard />
-                </div>
-                <div className="w-full md:w-1/2">
-                    <ContactCard />
+    <>
+        <SEO title={"Info"} />
+        <Layout>
+            <div className="container">
+                <MembershipCard />
+                <CurrentBoardCard />
+                <div className="flex flex-wrap">
+                    <div className="w-full md:w-1/2">
+                        <DocumentsCard />
+                    </div>
+                    <div className="w-full md:w-1/2">
+                        <ContactCard />
+                    </div>
                 </div>
             </div>
-        </div>
-    </Layout>
+        </Layout>
+    </>
 );
 
 export default InfoPage;
